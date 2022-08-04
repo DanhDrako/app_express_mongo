5f4b662997f8165e8e8ba7dcacfd3efd9d69c650
DanhDrako <nkocdanh58@gmail.com>
2022-08-04 18:35:33 +0700

    Commit

--- app.js
+++ app.js
@@ -34,6 +34,6 @@ app.get('/about',(req,res)=> res.render('about'))
 
 //Mang routes vao de su dung
 app.use('/posts',posts)
-const PORT= 5000
+const PORT= process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server khoi dong tai port ${PORT}`))
